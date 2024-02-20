95b416788e1084a43bea7270dff474a9c90a9882
LeandroRochAlg <leandrochaju@hotmail.com>
2024-02-20 20:34:10 -0300

    Update API base URL using Vite's import.meta.env

--- front/src/libs/api.ts
+++ front/src/libs/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",   // Import the API URL from the .env file using Vite's import.meta.env
+  baseURL: import.meta.env.VITE_API_URL,   // Import the API URL from the .env file using Vite's import.meta.env
 });
 
 api.interceptors.request.use((config) => {
