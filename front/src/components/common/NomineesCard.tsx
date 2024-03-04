696080e703b10b34cbfd523a61f1142e00470ebd
LeandroRochAlg <leandrochaju@hotmail.com>
2024-03-04 16:38:41 -0300

    Refactor NomineesCard and BetsComponent to support winners page

--- front/src/components/common/NomineesCard.tsx
+++ front/src/components/common/NomineesCard.tsx
@@ -1,53 +1,23 @@
 import { useState } from "react";
 import styles from "./NomineesCard.module.css";
-import api from "../../libs/api";
-import { AxiosError } from "axios";
 import { Category } from "../../types/Category";
 import Button from "./Button";
 import { Nominee } from "../../types/Nominee";
 
 interface NomineesCardProps {
   category: Category;
-  sendRoute?: string;
+  onClick: (selectedNominee: any) => void;
+  showBtn: boolean;
+  msg?: string;
 }
 
 const NomineesCard = (CardProps: NomineesCardProps) => {
-  const [msg, setMsg] = useState<string>("");
   const [selectedNominee, setSelectedNominee] = useState<number | null>(null);
-  const { category, sendRoute } = CardProps;
+  const { category, onClick, showBtn, msg } = CardProps;
 
-  const sendNominee = async () => {
-    if (selectedNominee === null) {
-      setMsg("Please select a nominee.");
-      return;
-    }
-
-    try {
-      const response = await api.post<string>(sendRoute || "/bet", {
-        nomineeId: selectedNominee,
-        categoryId: category._id,
-      });
-
-      if(response.status === 201 || response.status === 200){
-        setMsg("Nominee sent successfully.");
-
-        // Send back to the page so the user can see the updated bets
-        window.location.href = "/bets";
-        
-        // Reset the selected nominee
-        setSelectedNominee(null);
-      }else{
-        setMsg("An unexpected error occurred.");
-      }
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      setMsg(axiosError.response?.data as string || "An unexpected error occurred.");
-
-      if (axiosError.response?.status === 401 || axiosError.response?.status === 400) {
-        localStorage.removeItem("token");
-        window.location.href = "/login";
-      }
-    }
+  const sendNominee = () => {
+    onClick(selectedNominee);
+    setSelectedNominee(null);
   };
 
   return (
@@ -67,7 +37,7 @@ const NomineesCard = (CardProps: NomineesCardProps) => {
         ))}
       </div>
       <div className={styles.send}>
-        <Button onClick={sendNominee}>Send Nominee</Button>
+        {showBtn && <Button onClick={sendNominee}>Send Nominee</Button>}  {/* Shows the button if the showBtn prop is true */}
         <p>{msg}</p>
       </div>
     </div>
--- front/src/pages/Bets/BetsComponent.tsx
+++ front/src/pages/Bets/BetsComponent.tsx
@@ -6,6 +6,7 @@ import NomineesCard from '../../components/common/NomineesCard';
 import "../../styles/system.css"
 import styles from './BetsComponent.module.css';
 import api from '../../libs/api';
+import { AxiosError } from 'axios';
 import { Category } from '../../types/Category';
 import { GoArrowRight, GoArrowLeft } from "react-icons/go";
 
@@ -18,6 +19,40 @@ const BetsPage: React.FC = () => {
   });
   const [currentCategory, setCurrentCategory] = useState<Category>();
   const [loaded, setLoaded] = useState(false);
+  const [msg, setMsg] = useState('');
+
+  const sendNominee = async (selectedNominee: Number) => {
+    if (selectedNominee === null) {
+      setMsg("Please select a nominee.");
+      return;
+    }
+
+    const category = currentCategory as Category;
+
+    try {
+      const response = await api.post<string>("/bet", {
+        nomineeId: selectedNominee,
+        categoryId: category._id,
+      });
+
+      if(response.status === 201 || response.status === 200){
+        setMsg("Nominee sent successfully.");
+
+        // Send back to the page so the user can see the updated bets
+        window.location.href = "/bets";
+      }else{
+        setMsg("An unexpected error occurred.");
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError;
+      setMsg(axiosError.response?.data as string || "An unexpected error occurred.");
+
+      if (axiosError.response?.status === 401 || axiosError.response?.status === 400) {
+        localStorage.removeItem("token");
+        window.location.href = "/login";
+      }
+    }
+  };
 
   useEffect(() => {
     // Fetch categories from the API
@@ -62,7 +97,7 @@ const BetsPage: React.FC = () => {
       <Sidebar />
       <div className={styles.card}>
         <button onClick={navigateToPreviousCategory} className={styles.arrow}><GoArrowLeft /></button>
-        {loaded && <NomineesCard category={currentCategory as Category} />} {/* If available, pass the current category to the NomineesCard component */}
+        {loaded && <NomineesCard category={currentCategory as Category} msg={msg} onClick={sendNominee} showBtn={true}/>} {/* If available, pass the current category to the NomineesCard component */}
         <button onClick={navigateToNextCategory} className={styles.arrow}><GoArrowRight /></button>
       </div>
       <Footer />
