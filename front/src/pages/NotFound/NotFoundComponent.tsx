1f170e1b02aa903f01259f8d258f341d70db084a
LeandroRochAlg <leandrochaju@hotmail.com>
2024-03-05 18:49:46 -0300

    Update page titles and handle 404 page

--- front/src/pages/NotFound/NotFoundComponent.tsx
+++ front/src/pages/NotFound/NotFoundComponent.tsx
@@ -2,7 +2,7 @@ import '../../styles/auth.css';
 import styles from './NotFoundComponent.module.css';
 
 const NotFoundPage = () => {
-    return (
+    return (document.title = '404 - Not Found!') && (
         <div className='auth-body'>
             <h1 className={styles.title}>404 - Not Found!</h1>
         </div>
