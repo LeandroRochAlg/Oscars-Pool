dba01404478a866bf706c467f7f7c87cb68ea3b9
Leandro Rocha <leandrochaju@hotmail.com>
2025-02-07 12:51:59 -0300

    feat: Add nominees section to HomePage with localization support

--- front/src/pages/Home/HomeComponent.tsx
+++ front/src/pages/Home/HomeComponent.tsx
@@ -4,8 +4,27 @@ import Countdown from '../../components/common/CountDown';
 const HomePage = () => {
 	const { t } = useTranslation();
 
-	return (document.title = t('home'), 
-		<Countdown />
+	return (document.title = t('home'),
+		<div>
+			<Countdown />
+
+			<div className="card card-side bg-base-100 shadow-xl mx-2 md:w-[600px] md:mx-auto border border-primary my-5 text-base-200">
+				<div className="card-body">
+					<h2 className="card-title">{t('homePage.nominees.title')}</h2>
+					<p>{t('homePage.nominees.subtitle')}</p>
+					<div className="card-actions">
+						<button className="btn btn-primary" onClick={() => window.location.href = '/nominees'}>{t('homePage.nominees.viewAll')}</button>
+					</div>
+				</div>
+				<figure className="h-56 w-40">
+					<img
+						src="assets/images/Conclave.jpg"
+						alt="Movie"
+						className='h-full'
+					/>
+				</figure>
+			</div>
+		</div>
 	)
 };
 
