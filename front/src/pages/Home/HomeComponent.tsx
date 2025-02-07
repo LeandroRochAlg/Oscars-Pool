b8e772955d73a8fbc6a81965864a7014a846ee9d
Leandro Rocha <leandrochaju@hotmail.com>
2025-02-07 12:26:01 -0300

    feat: Add countdown component with localization for Oscar ceremony

--- front/src/pages/Home/HomeComponent.tsx
+++ front/src/pages/Home/HomeComponent.tsx
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import styles from './HomeComponent.module.css';
-import "../../styles/system.css"
+import { useTranslation } from 'react-i18next';
+import Countdown from '../../components/common/CountDown';
 
-const HomePage: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [available, setAvailable] = useState(false);
+const HomePage = () => {
+	const { t } = useTranslation();
 
-  useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user') || '{}');
-    setUsername(user.username || '');
-    setAvailable(true);
-  }, []);
-
-  return (document.title = 'Home',
-    <div className='system-body'>
-      <div className={styles.messageContainer}>
-        {available && <h2 className={styles.messageTitle}>Welcome, <span>{username}</span>!</h2>}
-        <p className={styles.message}>You are now logged into the Pool. Feel free to make a <a href="/bets">Bet</a> or see the <a href="/winners">Winners</a> or <a href="/leaderboard">Leaderboard</a> if available!</p>
-      </div>
-    </div>
-  );
+	return (document.title = t('home'), 
+		<Countdown />
+	)
 };
 
 export default HomePage;
