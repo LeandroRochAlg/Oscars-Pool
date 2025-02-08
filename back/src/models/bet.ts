a970ae4ec400566a676cffecefcc25edb05727b0
Leandro Rocha <leandrochaju@hotmail.com>
2025-02-08 19:53:16 -0300

    refactor: Update Bet and Pool models to include category and user management

--- back/src/models/bet.ts
+++ back/src/models/bet.ts
@@ -1,7 +1,4 @@
-import { Int32, ObjectId } from "mongodb";
-import { Nominee } from "./nominee";
-
 export interface Bet {
-    user: ObjectId;
-    nominees: Nominee[];
+    category: string;
+    nominees: string[];
 }
--- back/src/models/pool.ts
+++ back/src/models/pool.ts
@@ -1,12 +1,25 @@
-import { Int32, ObjectId } from "mongodb";
-import { CategoryPool } from "./categoryPool";
+import { ObjectId } from "mongodb";
+import { Bet } from "./bet";
+
+type UserPool = {
+    user: ObjectId;
+    admin: boolean;
+    bets?: Bet[];
+};
+
+export interface CategoryPool {
+    category: string;
+    weight: number;
+}
 
 export interface Pool {
     _id?: ObjectId;
     name: string;
-    description: string;
+    description?: string;
+    public: boolean;
+    inviteToken?: string;
     categories: CategoryPool[];
-    admins: ObjectId[];
-    users: ObjectId[];
-    weightSum: number;
+    users: UserPool[];
+    createdBy: ObjectId;
+    createdAt: Date;
 }
