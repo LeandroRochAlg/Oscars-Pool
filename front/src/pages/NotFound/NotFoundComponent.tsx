7ea7cd889b051d377278ee849f6efb31b44045f1
Leandro Rocha <leandrochaju@hotmail.com>
2025-02-24 20:03:11 -0300

    feat: Implement custom 404 Not Found page with localized messages and navigation

--- front/src/pages/NotFound/NotFoundComponent.tsx
+++ front/src/pages/NotFound/NotFoundComponent.tsx
@@ -1,9 +1,31 @@
-import styles from './NotFoundComponent.module.css';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const NotFoundPage = () => {
-    return (document.title = '404 - Not Found!') && (
-        <div className='auth-body'>
-            <h1 className={styles.title}>404 - Not Found!</h1>
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    return (document.title = t('404.title')), (
+        <div
+            className="hero min-h-screen"
+            style={{
+                backgroundImage: "url(/assets/images/WildRobot.jpg)",
+            }}>
+            <div className="hero-overlay bg-opacity-30 brightness-50"></div>
+            <div className="hero-content text-secondary text-center">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">{t("404.title")}</h1>
+                    <p className="mb-5">
+                        {t('404.message')}
+                    </p>
+                    <button 
+                        className="btn btn-success"
+                        onClick={() => navigate('/')}
+                    >
+                        {t('404.backToHome')}
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
